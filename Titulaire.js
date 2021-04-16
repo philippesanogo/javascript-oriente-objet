f4b17c769f390bd933f44836c2fa16e1a39432c7
philippe sanogo <philippe.sanogo@gmail.com>
2021-04-16 12:49:40 +0200

    ajout de la methode afficheTitulaireInfos() dans la class Titulaire

--- Titulaire.js
+++ Titulaire.js
@@ -2,9 +2,13 @@
  * Création d'une class Titulaire
  */
 
-class Titulaire{
+class Titulaire {
     constructor(nom, prenom) {
         this.nom = nom;
         this.prenom = prenom;
+    }
+
+    afficheTitulaireInfos() { 
+        console.log(`Le titualire du compte, s'appelle ${this.prenom} ${this.nom}`);
     }
 }
--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 /**
  * Création d'instances de la class Titulaire
- * Pour utiliser la class Titutlaire, je lie le fichier Titulaire.js dans index.html,
- * au-dessus de app.js.
+ * Pour utiliser la class Titutlaire, je lie le fichier Titulaire.js dans index.html, au-dessus de app.js.
  */
+
+// instance de la class Titulaire
 let titulaire1 = new Titulaire("Shade", "Tinem");
 console.log(titulaire1);
 
 let titulaire2 = new Titulaire("M.", "Philou");
 console.log(titulaire2);
+
+// utilise la methode créee dans le constructeur de la class Titulaire
+titulaire1.afficheTitulaireInfos();
+titulaire2.afficheTitulaireInfos();
